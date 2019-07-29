72a5fb0acbb5d1ff816b1bcc6ef11aec2abf4da4
Joshua <jschmidtnj@gmail.com>
2019-07-28 21:09:42 -0400

    trying to get progressive images working again

--- nuxt/plugins/progressive.ts
+++ nuxt/plugins/progressive.ts
@@ -1,9 +1,7 @@
 /**
  * progressive plugin required for progressive images
  */
-if (process.browser) {
-  // @ts-ignore
-  window.onNuxtReady(() => {
-    require('progressive-image.js')
-  })
-}
+// @ts-ignore
+window.onNuxtReady(() => {
+  require('progressive-image.js')
+})
